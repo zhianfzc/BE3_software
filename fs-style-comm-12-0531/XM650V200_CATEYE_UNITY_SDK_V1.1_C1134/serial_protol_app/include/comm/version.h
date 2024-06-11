f83b4b1b4fa944ead1ffe0b63d5dff08c269b4df
zhianfcz <fucz@zhiantec.com>
2024-06-11 17:16:57 +0800

    更改版本命名

--- fs-style-comm-12-0531/XM650V200_CATEYE_UNITY_SDK_V1.1_C1134/serial_protol_app/include/comm/version.h
+++ fs-style-comm-12-0531/XM650V200_CATEYE_UNITY_SDK_V1.1_C1134/serial_protol_app/include/comm/version.h
@@ -14,9 +14,9 @@ Copyright © zhian Tech Co., Ltd. 2022-2023. All rights reserved.
 	#define FIRMWARE_VERSION_ZA "BE3Y100_b231211"
 	#define FIRMWARE_VERSION_ZAPRD "ZF50S_BE3Y_V1.0.0_b231220"
 #else
-	#define FIRMWARE_VERSION "V1.1.3b"
-	#define FIRMWARE_VERSION_ZA "BE3113_b240607"
-	#define FIRMWARE_VERSION_ZAPRD "ZF50S_BE3_V1.1.3_b240607"
+	#define FIRMWARE_VERSION "ZAV01L"
+	#define FIRMWARE_VERSION_ZA "BE3_bZAV01L08_4611"
+	#define FIRMWARE_VERSION_ZAPRD "ZF50S_BE3_bZAV01L08_4611"
 #endif
 char g_version[32];
 char g_version_za[32];
