cbea8a9a94aa6d30263c067de9d1c6db3fd1be2c
zhianfcz <fucz@zhiantec.com>
2024-06-11 14:11:09 +0800

    同欣优化猫眼效果

---
+++ fs-style-comm-10X10-tx-0531/XM650V200_CATEYE_UNITY_SDK_V1.1_C1134/serial_protol_app/include/comm/version.h
@@ -0,0 +1,25 @@
+/*******************************************************************************
+Copyright © zhian Tech Co., Ltd. 2022-2023. All rights reserved.
+文件名: version.h
+作者@编号： zhian
+功能描述: version头文件
+******************************************************************************/
+#ifndef __VERSION_H__
+#define __VERSION_H__
+/*2024.4.17 异形板_裁剪uvc720P_3-RGB暗角*/
+#define BE3Y_T_VER  1 // if 1 BE3Y-T  , 0 BE3-T
+
+#if BE3Y_T_VER
+	#define FIRMWARE_VERSION "V1.0.3_bc"
+	#define FIRMWARE_VERSION_ZA "KT103_bc240607"
+	#define FIRMWARE_VERSION_ZAPRD "ZF50S_BE3KT_V1.0.3_bc240607"
+#else
+	#define FIRMWARE_VERSION "V1.1.3_b"
+	#define FIRMWARE_VERSION_ZA "BE3112_b240329"
+	#define FIRMWARE_VERSION_ZAPRD "ZF50S_BE3_V1.1.2_b240329"
+#endif
+char g_version[32];
+char g_version_za[32];
+
+#endif
+
